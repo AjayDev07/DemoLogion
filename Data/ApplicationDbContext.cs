49f6a4e39efa0afb3414dc0453687245224a4605
Diksha0007 <deeksha@creativebuffer.com>
2024-01-08 16:58:32 +0530

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LoginWithCrud.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace LoginWithCrud.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Login> Logins { get; set; }
+    }
+}
---
+++ Migrations/20231228062621_initiallll.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LoginWithCrud.Migrations
+{
+    public partial class initiallll : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
